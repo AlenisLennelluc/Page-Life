c38aaa08d60bed11bfcdbd7cb39ab0d1b38d478f
Dot <Willporterdesign@outlook.com>
2019-05-11 21:47:01 -0700

    Added Pause Function in Main.js

--- Page Life/phaser/PageLife/js/main.js
+++ Page Life/phaser/PageLife/js/main.js
@@ -9,3 +9,28 @@ game.state.add('MainMenu', MainMenu);
 game.state.add('Play', Play);
 game.state.add('GameOver', GameOver);
 game.state.start('MainMenu');
+
+
+//////////////////
+//PAUSE FUNCTION//
+//////////////////
+
+//CODE FROM NATHAN'S P2 PHYSICS CODE EXAMPLE
+// bind pause key to browser window event
+window.onkeydown = function(event) {
+	// capture keycode (event.which for Firefox compatibility)
+	var keycode = event.keyCode || event.which;
+	if(keycode === Phaser.Keyboard.P) {
+		pauseGame();
+	}
+}
+
+//CODE FROM NATHAN'S P2 PHYSICS CODE EXAMPLE
+function pauseGame() {
+	// toggle game pause
+	game.paused ? game.paused = false : game.paused = true;
+}
+
+/////////////////////
+//END OF PAUSE CODE//
+/////////////////////
